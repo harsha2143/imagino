991467fc7b3c10276f65c33040c35c45cbd46baa
harsha2143 <harshavardhansaripalli21@gamil.com>
2025-07-02 19:59:29 +0530

    update of testimonials cards

--- client/src/components/Testmonials.jsx
+++ client/src/components/Testmonials.jsx
@@ -9,13 +9,13 @@ const Testmonials = () => {
             transition={{ duration: 1, delay: 0.2 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true }}
-            className='flex flex-col items-center justify-center my-20 py-12'>
+            className='flex flex-col items-center justify-center my-20 py-12 '>
             <h1 className='text-3xl sm: text-4xl font-semibold mb-2'>Customer testimonials</h1>
             <p className='text-gray-500 mb-12'>What Our Users Are Saying</p>
-            <div className='flex flex-wrap gap-6'>
+            <div className='flex flex-wrap gap-6 ml-10 sm:ml-12 '>
                 {testimonialsData.map((testimonail, index) => (
                     <div key={index}>
-                        <div className='bg-white/20 p-12 rounded-lg shadow-md order w-80 m-auto cursor-pointer hover:scale-[1.02] transition-all' >
+                        <div className='bg-white/20 p-12 rounded-lg shadow-md order w-80 m-auto cursor-pointer hover:scale-[1.02] transition-all ' >
                             <img src={testimonail.image} alt="" className='rounded-full w-14' />
                             <h2 className='text-xl font-semibold mt-3'>{testimonail.name}</h2>
                             <p className='text-gray-500 mb-4'>{testimonail.role}</p>
